0ccac7224f41e4ff791e392b1e68a27acafd1494
RavinduKavinda <dlravindukavinda@gmail.com>
2024-03-18 14:14:48 +0530

    manage the form - out show in console

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+
+    {/* Initial state */}
+    const initialNote = {
+        title:"",
+        message:""
+    }
+
+    const [note, setNote] = useState (initialNote)
+
+    {/* handle the input changes */}
+    const handleChange = (e) => {
+        const{name,value} = e.target;
+
+        setNote(prevNote => (
+            {
+                ...note,
+                [name]:value
+            }
+        ));
+    };
+
+    {/* handle the form submission */}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(note);
+    };
+
   return (
     <section className="text-gray-400 bg-gray-900 body-font relative">
         <div className="container px-5 py-24 mx-auto">
@@ -15,7 +42,7 @@ const Home = () => {
             </div>
 
             {/* Form Details*/}
-            <div className="lg:w-1/2 md:w-2/3 mx-auto">
+            <form className="lg:w-1/2 md:w-2/3 mx-auto" onSubmit={handleSubmit}>
                 <div className="flex flex-wrap -m-2">
                     <div className="p-2 w-full">
                         <div className="relative">
@@ -27,7 +54,9 @@ const Home = () => {
                             id="title"
                             name="title"
                             className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-purple-500 focus:bg-gray-900 focus:ring-2 focus:ring-purple-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            />
+                            value={note.title}
+                            onChange={handleChange}
+                            required/>
                         </div>
                     </div>
 
@@ -43,18 +72,20 @@ const Home = () => {
                                 id="message"
                                 name="message"
                                 className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-purple-500 focus:bg-gray-900 focus:ring-2 focus:ring-purple-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                            />
+                                value={note.message}
+                                onChange={handleChange}
+                                required/>
                         </div>
                     </div>
 
-                    <div className="p-2 w-full">
+                    <div className="p-2 w-full" onSubmit={handleSubmit}>
                         <button className="flex mx-auto text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">
                             Submit
                         </button>
                     </div>
 
                 </div>
-            </div>
+            </form>
         </div>
     </section>
     )
