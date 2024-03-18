4f5e1a9f53d438bf589cb360fff933eac3ebaf35
RavinduKavinda <dlravindukavinda@gmail.com>
2024-03-18 15:39:36 +0530

    Route the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Home from './pages/Home'
 import Note from './pages/Note'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    < >
-      <Home/>
-      <Note/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/notes' element={<Note/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
