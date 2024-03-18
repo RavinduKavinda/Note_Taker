7bb41eb7bbe4f549f14a9611ba4ce3b14c2c04ec
RavinduKavinda <dlravindukavinda@gmail.com>
2024-03-18 16:33:43 +0530

    nav bar link to pages

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from './../assets/logo.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -10,10 +11,11 @@ const Header = () => {
                 <img src={logo} className=' w-[35px] h-[35px]' alt="" />
                 <span className="ml-3 text-3xl font-extrabold">Note Taker</span>
             </div>
+            
             {/* nav bar */}
             <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                <a className="mx-5 cursor-default hover:font-extrabold">First Link</a>
-                <a className="mx-5 cursor-default hover:font-extrabold">First Link</a>
+                <Link to={"/"} className="mx-5 cursor-default hover:font-bold hover:text-gray-800">Home</Link>
+                <Link to={"/notes"} className="mx-5 cursor-default hover:font-bold hover:text-gray-800">My Notes</Link>
             </nav>
         
         </div>
