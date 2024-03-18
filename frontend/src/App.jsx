d52332d9fb8cf7e9b7c2d79c42154d429a4cf06d
RavinduKavinda <dlravindukavinda@gmail.com>
2024-03-18 14:25:08 +0530

    added note page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Home from './pages/Home'
+import Note from './pages/Note'
 
 export default function App() {
   return (
     < >
       <Home/>
+      <Note/>
     </>
   )
 }
