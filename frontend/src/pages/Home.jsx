0699cf263aef5174e8dc3851f5e21ddd086c35ce
RavinduKavinda <dlravindukavinda@gmail.com>
2024-03-18 13:25:33 +0530

    home page form create

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="text-gray-400 bg-gray-900 body-font relative">
+        <div className="container px-5 py-24 mx-auto">
+            {/* Home Head */}
+            <div className="flex flex-col text-center w-full mb-12">
+                <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+                    Add A Note
+                </h1>
+                <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                    Please<br/>Fill the following form to add a new note.
+                </p>
+            </div>
+
+            {/* Form Details*/}
+            <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                <div className="flex flex-wrap -m-2">
+                    <div className="p-2 w-full">
+                        <div className="relative">
+                            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                                Title
+                            </label>
+                            <input
+                            type="text"
+                            id="title"
+                            name="title"
+                            className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-purple-500 focus:bg-gray-900 focus:ring-2 focus:ring-purple-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="p-2 w-full">
+                        <div className="relative">
+                            <label
+                                htmlFor="message"
+                                className="leading-7 text-sm text-gray-400"
+                            >
+                                Your Note
+                            </label>
+                            <textarea
+                                id="message"
+                                name="message"
+                                className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-purple-500 focus:bg-gray-900 focus:ring-2 focus:ring-purple-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="p-2 w-full">
+                        <button className="flex mx-auto text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">
+                            Submit
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default Home
