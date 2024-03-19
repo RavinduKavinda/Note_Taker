72d0c95bf5c9e39360bd1f2713fcd4d4b897f6da
RavinduKavinda <dlravindukavinda@gmail.com>
2024-03-19 16:35:52 +0530

    note controller setup

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,46 @@
+//add note
+const addNote = async(req,res) => {
+    try{
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+//get all notes
+const getAllNotes = async(req,res) => {
+    try{
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+//remove note
+const removeNotes = async(req,res) => {
+    try{
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+//edit note
+const editNotes = async(req,res) => {
+    try{
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+//get single note
+const getSingleNotes = async(req,res) => {
+    try{
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+module.exports = {addNote, getAllNotes, removeNotes, editNotes, getSingleNotes}
